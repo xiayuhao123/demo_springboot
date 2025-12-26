606a9d46f606998aa3bdd14451b65de14f92dec0
xyh <1094452235@qq.com>
2025-12-26 16:50:39 +0800

    修改了可以在本地运行的基本代码

--- src/main/java/controllor/HelloController.java
+++ src/main/java/controllor/HelloController.java
@@ -1,18 +1,19 @@
-package com.example.springboot_demo;
+package controllor;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind to.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 一个简单的 REST 控制器
  */
+@RestController
 public class HelloController {
 
     /**
      * 处理对 "/hello" 路径的 GET 请求
      * @return 返回一个简单的字符串
      */
-
+    @GetMapping("/hello")
     public String sayHello() {
         return "Hello, Spring Boot!";
     }
